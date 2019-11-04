a265a69d80258752ab80bfbac105beb68a75f84a
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-04 18:18:23 +0700

    read from file

--- prime_test.py
+++ prime_test.py
@@ -1,26 +1,11 @@
+import re
 from sympy import primerange
 from prime import primes
 
 
-def test_0():
-    assert len(primes(0)) == 0
-
-def test_1():
-    pl = primes(3)
-    assert len(pl) == 1
-    assert pl[0] == 2
-
-def _test_n(n):
-    pl = primes(n)
-    rpl = list(primerange(2, n))
+def test_prime():
+    with open("prime.txt", "r") as f:
+        pl = [int(s) for s in re.sub(r"[^0-9]+", " ", f.read()).split(" ")]
+    rpl = list(primerange(2, 10**7))
     assert len(pl) == len(rpl)
     assert all([a == b for a, b in zip(pl, rpl)])
-
-def test_100():
-    _test_n(100)
-
-def test_1e4():
-    _test_n(10**4)
-
-def test_1e7():
-    _test_n(10**7)
