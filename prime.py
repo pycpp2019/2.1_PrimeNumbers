aa3b99bcb99c058f8086668d030ebe82315ed32c
Name <reva00@ngs.ru>
2019-11-06 11:45:50 +0700

    version 1

---
+++ prime.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+N = 10**7 
+
+file = open('prime.txt', 'w')
+
+arr = np.zeros(N)
+lim = N**0.5
+
+j = 2
+while (j <= lim):
+    i = 2
+    while (j*i < len(arr)):
+        arr[j*i] = 1
+        i += 1
+    j += 1
+
+for i in range(1,N):
+    if (arr[i] == 0):
+       file.write(str(i)+' ')
