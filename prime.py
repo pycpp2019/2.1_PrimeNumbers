2eee74e6cc130aa1be62c00eba7e78bb6a853270
well120 <n.ushakova1@g.nsu.ru>
2019-12-09 23:33:16 +0700

    Added3!

--- prime.py
+++ prime.py
@@ -1,8 +1,8 @@
 from math import sqrt
 def func(n):
     lst = []
-    for i in range(3, n+1, 2):
-        if (i > 10) and (i%10==5):
+    for i in range(1, n+1, 2):
+        if (i > 10) and (i%10 == 5):
             continue
         for j in lst:
             if j > int((sqrt(i)) + 1):
@@ -13,7 +13,7 @@ def func(n):
         else:
             lst.append(i)
     return lst
-my_lst = func(10000000)
+my_lst = func(100)
 my_string = ', '.join(str(e) for e in my_lst)
 f = open('prime.txt', 'w')
 f.write(my_string)
