c80fb576a26630f5b2966d6cc810e61d9f2a48ab
Solovev-Sergey <55825616+Solovev-Sergey@users.noreply.github.com>
2019-12-26 14:36:07 +0700

    New

--- prime.py
+++ prime.py
@@ -1,29 +1,17 @@
-# программа выводит в файл 'prime.txt' все  простые числа до 10**7
 import numpy as np
+import math
+N=10**7
+A=np.arange(N)
 
-def simple_num(num, list):
-    ''' функция проверяет являетс число простым 
-    на вход число и список всех предшествующих простых чисел тип целые
-    на выходе         True - да,       False - нет
-    '''
-    if list.size == 0 :
-        return True
-    list2 = num % list
-    m = min(list2)
-    return bool(m)
-    
-def main(N):
-    '''функция сторит список из всех простых чисел меньших N и выводит их в файл 'prime.txt'
-    '''
-    list = np.array([])    #список всех предыдущих простых чисел
-    n = 2
-    file = open('prime.txt', 'w')
-    while (n <= N):
-        if simple_num(n, list):
-            file.write(str(n) +'\n')
-            print(n)
-            list = np.append(list,[n])
-        n += 1
-    file.close()
-
-main(10**7)
+for i in range(math.ceil(N/2)):
+    j=i+2
+    if (A[j]!=0):
+        while j<N-i-2:
+            A[j+i+2]=0
+            j+=i+2
+file = open('prime.txt', 'w')
+A[1]=0
+for i in range(N):
+    if A[i]!=0:
+        file.write(str(A[i])+' ')
+file.close()
